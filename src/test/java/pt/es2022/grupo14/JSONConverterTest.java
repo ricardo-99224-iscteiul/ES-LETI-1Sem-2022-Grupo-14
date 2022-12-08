cc2b55c86f492fb616df986fc9839df5adac7ab2
notCandeias66 <aefcs@iscte-iul.pt>
2022-12-08 20:27:37 +0000

    JSONConverterTest done

--- src/test/java/pt/es2022/grupo14/JSONConverterTest.java
+++ src/test/java/pt/es2022/grupo14/JSONConverterTest.java
@@ -1,41 +1,38 @@
 package pt.es2022.grupo14;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class JSONConverterTest
-{
+class JSONConverterTest {
+
+    JSONConverter jsonConverter;
 
     @BeforeEach
-    void setUp()
-    {
-    }
-
-    @AfterEach
-    void tearDown()
-    {
+    void setUp() {
+        jsonConverter = new JSONConverter();
     }
 
     @Test
-    void insert()
-    {
+    void insert() {
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert("username", null));
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert(null, "name"));
+
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert("username", null, "start", "end"));
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert(null, "name", "start", "end"));
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert("username", "name", null, "end"));
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.insert("username", "name", "start", null));
     }
 
     @Test
-    void testInsert()
-    {
+    void createFile() {
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.createFile(null));
     }
 
     @Test
-    void createFile()
-    {
-    }
+    void closeFile() {
+        assertThrows(IllegalArgumentException.class, () -> jsonConverter.closeFile(null));
 
-    @Test
-    void closeFile()
-    {
     }
 }
