30c3b3e64873e5c8eb22b0729a269fd39d6f3da7
notCandeias66 <aefcs@iscte-iul.pt>
2022-12-08 20:34:00 +0000

    JSONParserTest done

--- src/test/java/pt/es2022/grupo14/JSONParserTest.java
+++ src/test/java/pt/es2022/grupo14/JSONParserTest.java
@@ -1,26 +1,23 @@
 package pt.es2022.grupo14;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JSONParserTest
 {
+    JSONParser jsonParser;
 
     @BeforeEach
     void setUp()
     {
-    }
-
-    @AfterEach
-    void tearDown()
-    {
+        jsonParser = new JSONParser();
     }
 
     @Test
     void getAllEvents()
     {
+        assertThrows(IllegalArgumentException.class, () -> jsonParser.getAllEvents(null));
     }
 }
