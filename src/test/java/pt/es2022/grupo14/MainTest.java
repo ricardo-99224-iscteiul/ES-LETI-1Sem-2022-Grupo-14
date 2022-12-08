5e3a46935c8a99e0f8af6e900496df374d5da802
notCandeias66 <aefcs@iscte-iul.pt>
2022-12-08 20:51:05 +0000

    MainTest done

--- src/test/java/pt/es2022/grupo14/MainTest.java
+++ src/test/java/pt/es2022/grupo14/MainTest.java
@@ -1,7 +1,5 @@
 package pt.es2022.grupo14;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,23 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest
 {
 
-    @BeforeEach
-    void setUp()
-    {
-    }
-
-    @AfterEach
-    void tearDown()
-    {
-    }
-
     @Test
     void changeScreen()
     {
-    }
-
-    @Test
-    void getFrm()
-    {
+        assertThrows(IllegalArgumentException.class, () -> Main.changeScreen(null));
     }
 }
