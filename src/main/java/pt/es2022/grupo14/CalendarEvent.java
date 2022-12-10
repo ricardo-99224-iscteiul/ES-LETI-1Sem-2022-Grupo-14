69e83ff2c47464596c7235ebb60cd1fe4b5f73a2
rui-iscte <racjs@iscte-iul.pt>
2022-12-10 17:04:15 +0000

    Javadoc CalendarEvent

--- src/main/java/pt/es2022/grupo14/CalendarEvent.java
+++ src/main/java/pt/es2022/grupo14/CalendarEvent.java
@@ -14,10 +14,25 @@ public class CalendarEvent {
     private String text;
     private Color color;
 
+    /**
+     * Construtor da classe CalendarEvent
+     * @param date é a data do evento
+     * @param start é a hora do início do evento
+     * @param end é a hora do fim do evento
+     * @param text é a descrição do evento
+     */
     public CalendarEvent(LocalDate date, LocalTime start, LocalTime end, String text) {
         this(date, start, end, text, DEFAULT_COLOR);
     }
 
+    /**
+     * Construtor da classe CalendarEvent
+     * @param date é a data do evento
+     * @param start é a hora do início do evento
+     * @param end é a hora do fim do evento
+     * @param text é a descrição do evento
+     * @param color é a cor do evento
+     */
     public CalendarEvent(LocalDate date, LocalTime start, LocalTime end, String text, Color color) {
         if (date == null) throw new IllegalArgumentException("Date cannot be null");
         if (start == null) throw new IllegalArgumentException("Start time cannot be null");
@@ -32,51 +47,50 @@ public class CalendarEvent {
         this.color = color;
     }
 
+    /**
+     * @return da data do evento
+     */
     public LocalDate getDate() {
         return date;
     }
 
-    /*public void setDate(LocalDate date) {
-        this.date = date;
-    }*/
-
+    /**
+     * @return da hora do início do evento
+     */
     public LocalTime getStart() {
         return start;
     }
 
-    /*public void setStart(LocalTime start) {
-        this.start = start;
-    }*/
-
+    /**
+     * @return da hora do fim do evento
+     */
     public LocalTime getEnd() {
         return end;
     }
 
-    /*public void setEnd(LocalTime end) {
-        this.end = end;
-    }*/
-
+    /**
+     * @return da descrição do evento
+     */
     public String getText() {
         return text;
     }
 
-    /*public void setText(String text) {
-        this.text = text;
-    }*/
-
-    /*public String toString() {
-        return getDate() + " " + getStart() + "-" + getEnd() + ". " + getText();
-    }*/
-
+    /**
+     * @return da cor do evento
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+	 * Muda a cor do evento
+     * @param newColor é a nova cor do evento
+     */
     public void setColor(Color newColor) {
         if (newColor == null) throw new IllegalArgumentException("Color cannot be null");
         color = newColor;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -89,12 +103,4 @@ public class CalendarEvent {
         return end.equals(that.end);
 
     }
-
-    /*@Override
-    public int hashCode() {
-        int result = date.hashCode();
-        result = 31 * result + start.hashCode();
-        result = 31 * result + end.hashCode();
-        return result;
-    }*/
 }
