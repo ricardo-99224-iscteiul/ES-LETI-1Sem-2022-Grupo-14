f3bf45a3c81ec1eb577315667c59f2e56597d03f
ricardo-99224-iscteiul <rdlpo@iscte-iul.pt>
2022-12-14 17:25:54 +0000

    Added missing tests

--- src/test/java/pt/es2022/grupo14/CalendarScreenTest.java
+++ src/test/java/pt/es2022/grupo14/CalendarScreenTest.java
@@ -3,6 +3,10 @@ package pt.es2022.grupo14;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalendarScreenTest
@@ -16,15 +20,55 @@ class CalendarScreenTest
     }
 
     @Test
+    void showCalendar()
+    {
+        assertDoesNotThrow(() -> calendarScreen.showCalendar());
+    }
+
+    @Test
+    void updateCalendarFiles()
+    {
+        assertDoesNotThrow(() -> calendarScreen.updateCalendarFiles());
+    }
+
+    @Test
     void addEventsToCal()
     {
         assertThrows(IllegalArgumentException.class, () -> calendarScreen.addEventsToCal(null));
+    }
 
+    @Test
+    void updateEvents()
+    {
+        assertDoesNotThrow(() -> calendarScreen.updateEvents());
+    }
+
+    @Test
+    void updateEventsAvailability()
+    {
+        calendarScreen.showCalendar();
+        assertDoesNotThrow(() -> calendarScreen.updateEventsAvailability());
+    }
+
+    @Test
+    void existsEventsAt()
+    {
+        assertDoesNotThrow(() -> calendarScreen.existsEventAt(LocalDate.now(), LocalTime.now()));
     }
 
     @Test
     void changeColor()
     {
         assertThrows(IllegalArgumentException.class, () -> calendarScreen.changeColor(null));
+        ArrayList<CalendarEvent> events = new ArrayList<>();
+        CalendarEvent event = new CalendarEvent(
+                LocalDate.now(),
+                LocalTime.of(13, 0),
+                LocalTime.of(14, 0),
+                ""
+        );
+        events.add(event);
+
+        assertEquals(events, calendarScreen.changeColor(events));
     }
 }
