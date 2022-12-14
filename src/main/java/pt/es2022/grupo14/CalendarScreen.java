feda4adf09c80b898967476db7a1d4764db1f5a6
ricardo-99224-iscteiul <rdlpo@iscte-iul.pt>
2022-12-14 17:24:59 +0000

    Private to public in updateCalendarFiles() Removed unused imports

--- src/main/java/pt/es2022/grupo14/CalendarScreen.java
+++ src/main/java/pt/es2022/grupo14/CalendarScreen.java
@@ -9,10 +9,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -249,7 +246,7 @@ public class CalendarScreen
 	/**
 	 * Atualiza todos os calendarios
 	 */
-	private void updateCalendarFiles()
+	public void updateCalendarFiles()
 	{
 		ArrayList<String> jsonNames = new Utils().getCalendars();
 		JSONParser parser = new JSONParser();
